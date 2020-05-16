b46505533c7fb3bc471eb6dede930ea47ed4bd6f
Lee Priest <lpleepriest@gmail.com>
2020-05-16 17:09:51 +0100

    Add some logs, more generic naming

--- index.js
+++ index.js
@@ -2,34 +2,36 @@ require('dotenv').config();
 const ping = require('ping');
 const puppeteer = require('puppeteer');
 
-let skyCheckRunning = false;
+let checkRunning = false;
 
-const goToSkyLink = async () => {
-    skyCheckRunning = true;
+const goToLink = async () => {
+    checkRunning = true;
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(process.env.SKYLINK);
-    await page.waitForResponse(response => response.url() === 'https://www.sky.com/broadband/connection/reconnection/adsl' && response.status() === 200);
+    await page.goto(process.env.LINK);
+    await page.waitForResponse(response => response.url() === process.env.RESPONSELINK && response.status() === 200);
     return await ping.promise.probe(process.env.HOST);
 }
 
 const pingHost = async () => {
     setInterval(async () => {
-        if(skyCheckRunning) {
+        if(checkRunning) {
+            console.log('Check is running...')
             return;
         }
 
         const res = await ping.promise.probe(process.env.HOST, {timeout: 10});
 
         if (!res.alive) {
-            await goToSkyLink().then(skyRes => {
-                skyCheckRunning = false;
-                if (skyRes.alive) {
+            console.log('Connection is down, running the fix...')
+            await goToLink().then(linkRes => {
+                checkRunning = false;
+                if (linkRes.alive) {
                     return;
                 }
             }).catch(error => console.log(error))
         } else {
-            console.log(res)
+            console.log('Connection is active')
         }
     }, 1000)
 };
